bf042ad2d2203efa8855dd347599e87986fe6db4
mario <mario.gonzalez@hotmail.com.ar>
2022-09-04 22:13:10 -0300

    IOT

---
+++ IOT_esp8266/src/IOT_esp8266.cpp
@@ -0,0 +1,152 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <PubSubClient.h>
+
+const char *ssid = "TP-LINK_B33E";
+const char *password = "50868155";
+const char *mqtt_server = "casaiot.ddns.net";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+ESP8266WiFiMulti wifiMulti;
+
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+int Rebalse = 0;
+bool estado = false;
+bool estado1 = false;
+bool Valor_anterior = false;
+bool Valor_anterior1 = false;
+
+//------------------------------------------------------------------------------------
+// Libreria de funciones
+//------------------------------------------------------------------------------------
+void Llave_on() { client.publish("/tst/esp/", "on"); }
+void Llave_off() { client.publish("/tst/esp/", "off"); }
+void Led_off() { client.publish("/tst/esp/", "Eon"); }
+void Led_on() { client.publish("/tst/esp/", "Eoff"); }
+void Suscribe() { client.subscribe("/tst/app/"); }
+
+
+
+
+//------------------------------------------------------------------------------------
+// Libreria de tecla
+//------------------------------------------------------------------------------------
+void Tecla()
+{
+  bool Valor = digitalRead(4);
+  bool Valor1 = digitalRead(0);
+  Rebalse = 255;
+  while (Valor != Valor_anterior && Rebalse > 0)
+  {
+    Rebalse--;
+    delay(1);
+    if (Valor != Valor_anterior && Rebalse == 0)
+    { // Se compara la variable estadoPulsador
+      estado1 = false;
+      estado = !estado; // cambiamos el estado
+      Valor1 = !Valor1;
+      digitalWrite(5, estado); // escribimos el nuevo valor
+      digitalWrite(0, Valor1); // escribimos el nuevo valor
+      if (Valor1 == 1)
+      {
+        Llave_on();
+        Led_on();
+        delay(50);
+      }
+      else
+      {
+        Llave_off();
+        Led_off();
+        delay(50);
+      }
+      Valor_anterior = Valor;
+      delay(255);
+    }
+  }
+}
+//------------------------------------------------------------------------------------
+// Libreria de funciones varias Setup WIFI
+//------------------------------------------------------------------------------------
+void setup_wifi()
+{
+  delay(10);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    //Tecla();
+    //delay(1000);
+  }
+  digitalWrite(2, HIGH);
+}
+
+//------------------------------------------------------------------------------------
+// Libreria de funciones Reconexion
+//------------------------------------------------------------------------------------
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Tecla();
+    String clientId = "Luz-TST";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str()))
+    {
+      Led_off();
+      Suscribe();
+    }
+  }
+}
+//------------------------------------------------------------------------------------
+// Libreria de funciones Callback
+//------------------------------------------------------------------------------------
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    digitalWrite(0, 0);   // Rele
+  if ((char)payload[1] == 'n')
+  {
+    digitalWrite(0, 1); // escribimos el nuevo valor
+    Led_on();
+    delay(200);
+  }
+  if ((char)payload[1] == 'f')
+  {
+    digitalWrite(0, 0); // escribimos el nuevo valor
+    Led_off();
+    delay(200);
+  }
+}
+//------------------------------------------------------------------------------------
+// Funcion Setup
+//------------------------------------------------------------------------------------
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(4, INPUT);    // Llave
+  digitalWrite(0, 1);   // Rele
+  pinMode(5, OUTPUT);   // Pin Led Amarillo del Link
+  digitalWrite(5, LOW); //
+  Serial.begin(9600);
+  setup_wifi();
+  digitalWrite(2, HIGH);
+  client.setServer(mqtt_server, 1883); // conecto con el server MQTT al puerto 1883
+  client.setCallback(callback);
+}
+//------------------------------------------------------------------------------------
+// Loop infinito
+//------------------------------------------------------------------------------------
+void loop()
+{
+Serial.print(" hola por aca pase");
+delay(520);
+  // Tecla();
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+}
